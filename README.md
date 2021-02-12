2a95a1c6b77065d6e7760a295bf00223ee76138d
Aaaaiiiiiee <wjjgjk@naver.com>
2021-02-12 11:23:44 +0000

    Add Multiple upload function (1)

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 
 # TODO
 - [x] Save sql query so that you can easily make database in another computer.
+- [ ] Make Upload function
+    * https://github.com/expressjs/multer/blob/master/README.md
+    * https://stackoverflow.com/questions/39350040/uploading-multiple-files-with-multer
+    * Search `how can i use multer by multiple upload`
+    
 - [ ] what to do
 
 * [Add Login function](https://github.com/Aaaaiiiiiee/family_blog_practice/commit/85781019622bdefd4c0ad1e584f2f984ebc3ab0e)
--- router/upload.js
+++ router/upload.js
@@ -4,21 +4,28 @@ var template = require('../lib/template.js');
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest: './public/album/'});
+var storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './public/album')
+    },
+    filename: function(req, file, callback){
+        callback(null, file.filename + '-' + Date.now())
+    }
+});
+var upload = multer({storage: storage});
 
 router.get('/', (req, res)=>{
     if(!req.session.is_logined) res.redirect('/login');
     else{
-        var html = template.header('');
-        html += `
-            <form action="/upload/stats" enctype="multipart/form-data" method="post">
+        var html = template.header(``, `
+            <form action="/upload/multiple-upload" enctype="multipart/form-data" method="post">
                 <div class="form-group">
-                    <input type="file" name="uploaded_file">
-                    <input type="text" placeholder="Number of speakers" name="nspeakers">
-                    <input type="submit" value="Get me the stats!">            
+                    <input type="file" class="form-control-file" name="uploaded_file_1" multiple>
+                    <input type="text" class="form-control" placeholder="Number of speakers" name="nspeakers">
+                    <input type="submit" value="Get me the stats!" class="btn">
                 </div>
             </form>
-        `;
+        `);
         res.send(html);
     }
 });
@@ -30,5 +37,7 @@ router.post('/stats', upload.single('uploaded_file'), function (req, res) {
    console.log("and");
    console.log(req.body);
 });
+
+router.post('/multiple-upload', upload.array('photos', 10), (req, res, next)=>{});
 
 module.exports = router;
